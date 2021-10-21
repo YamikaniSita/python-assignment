6f6b7034eeab392f2a27a8402fa6529f10c50ef8
YamikaniSita <sitayamikani@gmail.com>
2021-10-21 12:37:27 +0100

    comments

--- guessing-game.py
+++ guessing-game.py
@@ -1,17 +1,17 @@
-##I attempt to guess a number using the key pressed by the user to enter the game each key value
-##has its own assigned value shuffled stored in the dictionary.
+"""I attempt to generate a pseudo random number by asking the user to press any number in order to enter the game
+This number is what determines the guessed key word from the dictionary below each number has its owned assigned number"""
 
 keys = {'0': 8, '1':7, '2': 1, '3': 0, '4':9, '5':4, '6':2, '7':5, '8':3, '9': 6}
 try:
     #this try block is to prevent user from entering a character
-    pressedKey = int(input('We guess a number you try to find it, press any key and press enter to continue: '))
+    pressedKey = int(input('We guess a number you try to find it, press any number then press enter to continue: '))
 except ValueError as error:
     print('Check input: ', error)
 else:
     guess = keys[str(pressedKey)]
     guessedValue = False
     while(guessedValue == False):
-        cuGuess = int(input('Enter guess'))
-        if cuGuess == guess:
+        currentGuess = int(input('Enter guess'))
+        if currentGuess == guess:
             guessedValue = True
     print("Guessed right the number was {}..congrats".format(guess))
